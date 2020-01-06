591e1657091200f90c338faeaa8ed4d16efaca54
LeoRoma <xiajtn@gmail.com>
2020-01-06 11:42:47 +0000

    testing button

---
+++ src/converter.js
@@ -0,0 +1,38 @@
+notes = {
+  0: {
+    note: "C",
+    octave: 0,
+    frequency: 16.35,
+  },
+  1: {
+    note: "C#/Db",
+    octave: 0,
+    frequency: 17.32
+  },
+  2: {
+    note: "D",
+    octave: 0,
+    frequency: 18.35
+  }
+}
+
+$(document).ready(function () {
+
+  $(".submit").click(function (event) {
+    let num = $("input").first().val();
+    console.log(num);
+    console.log(notes[num]);
+    console.log(notes[num].note)
+    console.log(notes[num].octave)
+    console.log(notes[num].frequency)
+    // if ($("input").first().val() === `${total}`) {
+    //   // $( "span" ).text( "Validated..." ).show();
+    //   alert("Thank you for your purchase")
+    //   location.reload(true);
+    //   return
+    // }
+    // event.preventDefault;
+    // alert('Please insert correct amount')
+  });
+
+});
