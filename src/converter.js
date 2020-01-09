4e9423fb09595432dc59e0a01e9fd358e102b7cb
LeoRoma <xiajtn@gmail.com>
2020-01-09 11:31:46 +0000

    minor chord added

--- src/converter.js
+++ src/converter.js
@@ -246,7 +246,7 @@ $(document).ready(function () {
   $(".submit-chord-min").click(function (event) {
     let chord = $("#chord-min").first().val();
     console.log(`${scale[scale.indexOf(chord)]} + ${scale[scale.indexOf(chord) + 2]} + ${scale[scale.indexOf(chord) + 4]}`);
-    $resultChordMin.append('<p>Your chord is: ' + `${scale[scale.indexOf(chord)]} ${scale[scale.indexOf(chord) + 2]} ${scale[scale.indexOf(chord) + 4]}` + '<p>');
+    $resultChordMin.append('<p>Your chord is: ' + `${scale[scale.indexOf(chord)]} ${scale[scale.indexOf(chord) + 3]} ${scale[scale.indexOf(chord) + 7]}` + '<p>');
   I});
 
 });
