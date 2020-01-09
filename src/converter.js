f132976e355d38eba693a707cafcd52c95bcf906
LeoRoma <xiajtn@gmail.com>
2020-01-09 10:52:02 +0000

    scale array added

--- src/converter.js
+++ src/converter.js
@@ -209,6 +209,10 @@ numbers = {
   }
 }
 
+scale = ['C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab', 'A', 'A#/Bb', 'B'] 
+
+console.log(scale['C'])
+
 $(document).ready(function () {
 
   var $resultNote = $('#result-note');
